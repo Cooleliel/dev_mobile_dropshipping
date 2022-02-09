6c318a75d33ae0c2e9d5a935918a0c6d8059d49e
CesaireX <cesairetoe@gmail.com>
2022-02-09 18:25:49 +0000

    Bases ok

---
+++ README.md
@@ -0,0 +1,1 @@
+# dev_mobile_dropshipping
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'inscription',
+    loadChildren: () => import('./pages/inscription/inscription.module').then( m => m.InscriptionPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
