1ee74669af8b64f0b5442800ae95cfb7110c69c3
Cooleliel <elielcoulibaly39@gmail.com>
2022-02-12 17:35:17 +0000

    Declaration des models Produit et Categorie

---
+++ src/app/models/categorie.model.ts
@@ -0,0 +1,5 @@
+//Declaration de la classe Categorie dans le dossier models
+export interface Categorie{
+ title: string ;
+ image: string ;
+}
---
+++ src/app/models/produit.model.ts
@@ -0,0 +1,10 @@
+//Declaration de la classe Produit dans le dossier models
+
+export interface Produit{
+ id: number ;
+ name: string ;
+
+ price: number;
+ imageUrl: string ;
+ liked: boolean;
+}
