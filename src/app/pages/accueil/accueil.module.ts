25681808c7fe0320f3202035eb9a885743326b8a
Cooleliel <elielcoulibaly39@gmail.com>
2022-02-12 17:43:26 +0000

    Importations des modules des composants accueil-slide-header , categorie-card , produit-card , produit-slide

--- src/app/pages/accueil/accueil.module.ts
+++ src/app/pages/accueil/accueil.module.ts
@@ -7,14 +7,22 @@ import { IonicModule } from '@ionic/angular';
 import { AccueilPageRoutingModule } from './accueil-routing.module';
 
 import { AccueilPage } from './accueil.page';
+import { AccueilSlideHeaderModule } from 'src/app/composants/accueil-slide-header/accueil-slide-header.module';
+import { CategorieCardModule } from 'src/app/composants/categorie-card/categorie-card.module';
+import { ProduitSlideModule } from 'src/app/composants/produit-slide/produit-slide.module';
 
 @NgModule({
+  //importation ou appel des differentes Modules qui vont servis ou interagir avec  la vue de la page AccueilPage(html)
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AccueilPageRoutingModule
+    AccueilPageRoutingModule,
+    AccueilSlideHeaderModule,
+    CategorieCardModule,
+    ProduitSlideModule
   ],
-  declarations: [AccueilPage]
+
+  declarations: [AccueilPage]//declaration ou appel de la vue AccueilPage qui va permettre d'utiliser les differents modules
 })
 export class AccueilPageModule {}
