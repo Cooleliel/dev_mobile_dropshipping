ca6f32a343b2980752001cf043ffc419272f799d
Cooleliel <elielcoulibaly39@gmail.com>
2022-02-13 01:41:11 +0000

    Declaration d'un evenement de sortie et d'une fonction getProduitById()

--- src/app/composants/produit-card/produit-card.component.ts
+++ src/app/composants/produit-card/produit-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Produit } from 'src/app/models/produit.model';
 
 @Component({
@@ -7,6 +7,13 @@ import { Produit } from 'src/app/models/produit.model';
   styleUrls: ['./produit-card.component.scss'],
 })
 export class ProduitCardComponent {
-  @Input()  produitCard:  Produit;
+  @Input()  produitCard:  Produit;//declaration d'objet de type Produit en entree(recoit des donnees de son composant parent)
+  @Output() clicked = new EventEmitter();//declaration de variable emettant un evenement en sortie(transmet des donnees a son composant parent) 
+
+  //la fonction getProduitById() utilise la variable de soirte clicked pour declencher un evenement avec la valeur idProduit
+  getProduitById(idProduit: any)  {
+    this.clicked.emit(idProduit)  ;
+  }
 }
+
 
